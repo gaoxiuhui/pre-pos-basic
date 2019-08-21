00555ec4804779e9eb47e409ad808ad263ac78e4
luna <email@example.com>
2019-08-21 10:21:00 +0800

    修改

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,10 @@
 'use strict';
 
 function find(collection, element) {
-  return '实现练习要求，并改写该行代码。';
+  for(let i=0;i<collection.length;i++){
+    if(collection[i]=== element){
+      return collection[i];
+    }
+  }
+  return "查无此数";
 }
